d4efa0fedac3d689ff29d0a3b03a759d4f816456
Evgeniy Galyamov <galyamov.evgenyi@gmail.com>
2025-03-17 01:57:01 +0300

    1 attempt

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/ekorol/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+// Вывод всех карточек
+initialCards.forEach(function(item){
+  placesList.append(createCard(item, deleteCard));
+});
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(content, deleteCard) {
 
-// @todo: Функция удаления карточки
+  // Клонируем темплейт
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: Вывести карточки на страницу
+  // Подставляем данные в темплейт
+  cardElement.querySelector('.card__image').src = content.link;
+  cardElement.querySelector('.card__title').textContent = content.name;
+
+  // Добавляем обработчик кнопки удалить
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', function() {
+    deleteCard(cardElement);
+  });
+
+  // Возвращаем подготовленный для вставки шаблон
+  return cardElement;
+};
+
+// Функция удаления карточки
+function deleteCard(card) {
+  card.remove();
+}
